77e17c2e4ad556959971ac15d2fc49bd65b3c2e5
arbazpatel <patelarbaz75@gmail.com>
2021-05-29 19:59:21 +0530

    permissions

--- api/views.py
+++ api/views.py
@@ -17,12 +17,20 @@ class UserViewSet(viewsets.ModelViewSet):
     # Add this code block
     def get_permissions(self):
         permission_classes = []
+        # if self.action == 'create':
+        #     permission_classes = [AllowAny]
+        # elif self.action == 'retrieve' or self.action == 'update' or self.action == 'partial_update':
+        #     permission_classes = [IsLoggedInUserOrAdmin]
+        # elif self.action == 'list' or self.action == 'destroy':
+        #     permission_classes = [IsAdminUser]
+        # return [permission() for permission in permission_classes]
+
         if self.action == 'create':
             permission_classes = [AllowAny]
         elif self.action == 'retrieve' or self.action == 'update' or self.action == 'partial_update':
-            permission_classes = [IsLoggedInUserOrAdmin]
+            permission_classes = [AllowAny]
         elif self.action == 'list' or self.action == 'destroy':
-            permission_classes = [IsAdminUser]
+            permission_classes = [AllowAny]
         return [permission() for permission in permission_classes]
 
 class FeedbackApiView(APIView):
