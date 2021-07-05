bee251e7e84be794c82c6f547208f53aca4748c6
arbazpatel <patelarbaz75@gmail.com>
2021-07-05 11:44:49 +0530

    long lat

--- api/admin_views/adminviews.py
+++ api/admin_views/adminviews.py
@@ -207,14 +207,14 @@ class AdminSetupApiView(generics.ListAPIView):
 class AdminUserApiView(generics.ListAPIView):
     # permission_classes = [IsAuthenticated]
     def get_queryset(self):
-        queryset = UserProfile.objects.filter()
+        queryset = User.objects.filter()
         setup = self.request.GET.get('setup')
         if setup:
             queryset = queryset.filter(setup=setup)
         return queryset
     filter_backends = [SearchFilter,]
-    serializer_class = UserProfileSerializer
-    search_fields  = ('name','email','gender','mobile','age')
+    serializer_class = UserSerializer
+    search_fields  = ('name','mobile','email','gender','mobile','age')
 
 class AdminNotificationAdminApiView(generics.ListAPIView):
     def get_queryset(self):
